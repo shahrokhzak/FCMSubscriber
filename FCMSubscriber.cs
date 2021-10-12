38238c7a69eb05e879876064e5b5daace6f8ab1f
Shahrokh Zakipour <shahrokh.zakipoor@gmail.com>
2021-10-12 09:58:33 +0330

    Add project files.

---
+++ FCMSubscriber.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+
+namespace FCMSubscriber
+{
+    internal class FCMSubscriber
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="token">single token to register to topic </param>
+        /// <param name="topicEnum">
+        ///     1=all 2=develop 3=backendTest 4=androidTest 1-send to all user this is for live scenario purpose 2-send
+        ///     to beta tester user 3-back end test user 4-android test user
+        /// </param>
+        /// <param name="googleCredential">Address To the fire base credential JSON file </param>
+        /// <returns></returns>
+        public static async Task<string> Subscribe(string token, Enum topicEnum, string googleCredential)
+        {
+            var result = "";
+            if (FirebaseApp.DefaultInstance == null)
+            {
+                var defaultApp = FirebaseApp.Create(new AppOptions
+                {
+                    //this path should point to desired credential of project.
+                    Credential =
+                        GoogleCredential.FromFile(googleCredential)
+                });
+                Console.WriteLine(defaultApp.Name);
+            }
+
+            var tokenList = new List<string>();
+            tokenList.Add(token);
+            var response = await FirebaseMessaging.DefaultInstance.SubscribeToTopicAsync(
+                tokenList, topicEnum.ToString());
+            result =
+                $"Success Subscribe:{response.SuccessCount} . failure Subscribe: {response.FailureCount} . Error:{response.Errors} \r\n";
+
+            return result;
+        }
+
+        /// <summary>
+        ///     For Subscribe List Of Token(FCM Tokens) To Specific Topic.
+        /// </summary>
+        /// <param name="tokenList">list of token for registering into topic </param>
+        /// <param name="topicEnum">
+        ///     1=all 2=develop 3=backendTest 4=androidTest 1-send to all user this is for live scenario purpose 2-send
+        ///     to beta tester user 3-back end test user 4-android test user
+        /// </param>
+        /// <param name="googleCredential">Address To the firebase credential JSON file </param>
+        /// <returns></returns>
+        public static async Task<string> Subscribe(List<string> tokenList, Enum topicEnum, string googleCredential)
+        {
+            var result = "";
+            var tokenChunk = new List<string>();
+            if (FirebaseApp.DefaultInstance == null)
+            {
+                var defaultApp = FirebaseApp.Create(new AppOptions
+                {
+                    //this path should point to desired credential of project.
+                    Credential =
+                        GoogleCredential.FromFile(googleCredential)
+                });
+                Console.WriteLine(defaultApp.Name);
+            }
+
+            TopicManagementResponse response;
+            if (tokenList.Count > 1000)
+            {
+                var count = 0;
+
+                foreach (var token in tokenList)
+                {
+                    tokenChunk.Add(token);
+                    count++;
+                    if (tokenChunk.Count >= 999)
+                    {
+                        count = 0;
+
+                        response = await FirebaseMessaging.DefaultInstance.SubscribeToTopicAsync(
+                            tokenChunk, topicEnum.ToString());
+                        result +=
+                            $"Success Subscribe:{response.SuccessCount} . failure Subscribe: {response.FailureCount} . Error:{response.Errors} \r\n";
+                        tokenChunk.Clear();
+                    }
+                }
+
+                response = await FirebaseMessaging.DefaultInstance.SubscribeToTopicAsync(tokenChunk,
+                    topicEnum.ToString());
+                result +=
+                    $"Success Subscribe:{response.SuccessCount} . failure Subscribe: {response.FailureCount} . Error:{response.Errors} \r\n";
+                tokenChunk.Clear();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        ///     to fetch and read fcm token directly from DB can use this.
+        /// </summary>
+        /// <param name="connectionString">connection string to desired database</param>
+        /// <param name="tableName">name of the table that FCM Token Are stored to </param>
+        /// <param name="FCMColumnName">name of the column that FCM Tokens are stored to </param>
+        /// <param name="topicEnum">
+        ///     1=all 2=develop 3=backendTest 4=androidTest 1-send to all user this is for live scenario purpose 2-send
+        ///     to beta tester user 3-back end test user 4-android test user
+        /// </param>
+        /// <param name="googleCredential">Address To the fire base credential JSON file </param>
+        /// <returns></returns>
+        public static async Task<string> Subscribe(string connectionString, string tableName, string FCMColumnName,
+            Enum topicEnum
+            , string googleCredential)
+        {
+            var tokenList = new List<string>();
+
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                using (var command =
+                    new SqlCommand($"select {FCMColumnName} from {tableName} where {FCMColumnName} is not null",
+                        connection))
+                {
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read()) tokenList.Add(reader[0].ToString());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            if (tokenList.Count < 1) return "No DB Record Found!!!";
+
+            if (FirebaseApp.DefaultInstance == null)
+            {
+                var defaultApp = FirebaseApp.Create(new AppOptions
+                {
+                    //this path should point to desired credential of project.
+                    Credential =
+                        GoogleCredential.FromFile(googleCredential)
+                });
+                Console.WriteLine(defaultApp.Name);
+            }
+
+            var tokenChunk = new List<string>();
+            var result = "";
+            TopicManagementResponse response;
+
+            if (tokenList.Count > 1000)
+            {
+                var count = 0;
+
+                
+                foreach (var token in tokenList)
+                {
+                    tokenChunk.Add(token);
+                    count++;
+                    if (tokenChunk.Count >= 999)
+                    {
+                        count = 0;
+
+                        response = await FirebaseMessaging.DefaultInstance.SubscribeToTopicAsync(
+                            tokenChunk, topicEnum.ToString());
+                        result +=
+                            $"Success Subscribe:{response.SuccessCount} . failure Subscribe: {response.FailureCount} . Error:{response.Errors} \r\n";
+                        tokenChunk.Clear();
+                    }
+                }
+
+                response = await FirebaseMessaging.DefaultInstance.SubscribeToTopicAsync(tokenChunk,
+                    topicEnum.ToString());
+                result +=
+                    $"Success Subscribe:{response.SuccessCount} . failure Subscribe: {response.FailureCount} . Error:{response.Errors} \r\n";
+                tokenChunk.Clear();
+            }
+            else
+            {
+                response = await FirebaseMessaging.DefaultInstance.SubscribeToTopicAsync(
+                    tokenList, topicEnum.ToString());
+                result +=
+                    $"Success Subscribe:{response.SuccessCount} . failure Subscribe: {response.FailureCount} . Error:{response.Errors} \r\n";
+                tokenChunk.Clear();
+            }
+            return result;
+        }
+
+        internal enum TopicName
+        {
+            All = 1,
+            Develop = 2,
+            BackendTest = 3,
+            AndroidTest = 4
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FCMSubscriber;
+
+namespace FCMSubscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var topicName = FCMSubscriber.TopicName.All;
+            
+            var token =
+                "c18gz7uLTWm_CB3CNOPAHw:APA91bE_fmDzUowDjtw7afE1t4xsZjMFpYi2pKi01RiMtL6-jxZfH4Ybx68BaRvwIA9ICs4LT1j4lpR4e1eAYVWl-moJ7VkuY5j_ec1vEnoXW7bALUODVM3bOcpEjTMUqCaedq_mGQ-4";
+
+            var TokensList = new List<string>
+            {
+                
+                "c18gz7uLTWm_CB3CNOPAHw:APA91bE_fmDzUowDjtw7afE1t4xsZjMFpYi2pKi01RiMtL6-jxZfH4Ybx68BaRvwIA9ICs4LT1j4lpR4e1eAYVWl-moJ7VkuY5j_ec1vEnoXW7bALUODVM3bOcpEjTMUqCaedq_mGQ-4",
+                "cnntxr78SkSR1TqVTV7rAm:APA91bG84B83wMmvidqgN31G6ewF7Y8MdR_KTg6QAzLs9xrL5LG4npaO_uhvJ1FXuYFf5c2DecJ4_nTeHgfUt6ScIq48ibkGxNqLYz5oID0B0cOf9rj8hN-jA1i65-TnDgoBIZ6HOL3E",
+                "ekllUeA2TSKY1k2F1CK5z_:APA91bFCxmaeR9kjb3kD_tpo3s6D44IPe0tThm1YOWYC6D-FfWAV81-kZLawxwAYsp5rvDHAnzJDUuq52Lj-qIs1anhtDMtugFM6y3-mv3EXx6-6bp3bo-_TSr6zqGG8qO0bQ_t-oz_I",
+                "cu_eDLYjR5G7YfurM4syQ6:APA91bEh5sCXw56a2AxxopMfTKr5e3BtBS0GBKmT9CQ4s7e2uBp7Q2NHLWT_MCwKydhiRA4YXFnvpisd1ve4jU1cpDuxL1AOZtNbuO5WQjOb3hoDVY03u3oCJkPJ0T--l1SAvJAzgilO"
+                
+            };
+
+            var connection = "Server=10.0.79.36;Database=PNA_SunService;User Id=sa; Password=abc@1234;Trusted_Connection=false;";
+
+
+            //SAMPLES
+            
+            //var result = FCMSubscriber.Subscribe("asdasdasdasdasdasd", topicName,@"D:\sun3test-firebase-adminsdk-xaoio-42fe26a1fb.json");
+            //var result = FCMSubscriber.Subscribe("asdasdasdasdasdasd", topicName,@"D:\sun3test-firebase-adminsdk-xaoio-42fe26a1fb.json");
+            //var result = FCMSubscriber.Subscribe(connection, "customer", "FcmToken", topicName,@"D:\sun3test-firebase-adminsdk-xaoio-42fe26a1fb.json");
+
+
+
+            Console.WriteLine(result.Result);
+            Console.WriteLine("*********************");
+            Console.ReadLine();
+        }
+    }
+}
